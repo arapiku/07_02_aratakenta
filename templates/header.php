ae5cc7c7c9babfa120416a7e6591d5a1d627bd6d
arapiku <arpaiku420@gmail.com>
2018-01-25 22:37:37 +0900

    ヘッダーファイルの作成

---
+++ templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BookApp</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<header>
+    <p class="logo"><a href="index.php">BookApp</a></p>
+    <p class="logout"><a href="logout.php">ログアウト</a></p>
+</header>
